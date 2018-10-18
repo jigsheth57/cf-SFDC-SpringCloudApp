142836ee29ab3c2ada5daac7cdd107f2ab60d9de
Jignesh Sheth <jsheth@pivotal.io>
2018-10-17 20:23:34 -0500

    update

--- build-cloud-native-images.sh
+++ build-cloud-native-images.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -eu
 
-pack build jigsheth57/configserver:latest --path ./configServer --publish
+pack build jigsheth57/configserver:latest --path ./configServer
 
-pack build jigsheth57/eurekaserver:latest --path ./eurekaServer --publish
+pack build jigsheth57/eurekaserver:latest --path ./eurekaServer
 
-pack build jigsheth57/hystrixdashboard:latest --path ./hystrixDashboard --publish
+pack build jigsheth57/hystrixdashboard:latest --path ./hystrixDashboard
 
-pack build jigsheth57/authservice:latest --path ./authService --publish
+pack build jigsheth57/authservice:latest --path ./authService
 
-pack build jigsheth57/accountservice:latest --path ./accountService --publish
+pack build jigsheth57/accountservice:latest --path ./accountService
 
-pack build jigsheth57/contactservice:latest --path ./contactService --publish
+pack build jigsheth57/contactservice:latest --path ./contactService
 
-pack build jigsheth57/opportunityservice:latest --path ./opportunityService --publish
+pack build jigsheth57/opportunityservice:latest --path ./opportunityService
 
-pack build jigsheth57/gatewayservice:latest --path ./gatewayService --publish
+pack build jigsheth57/gatewayservice:latest --path ./gatewayService
 
-pack build jigsheth57/sfdcwebapp:latest --path ./sfdcwebapp --publish
+pack build jigsheth57/sfdcwebapp:latest --path ./sfdcwebapp
 
-pack build jigsheth57/dataloader:latest --path ./dataloader --publish
+pack build jigsheth57/dataloader:latest --path ./dataloader 
